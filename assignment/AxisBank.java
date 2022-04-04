7c208b6d4b05bbe0ff54809900dcae487c6b9246
MangaiPalaniappan <50473832+MangaiPalaniappan@users.noreply.github.com>
2022-04-04 06:32:14 -0700

    Add files via upload

---
+++ assignment/AxisBank.java
@@ -0,0 +1,15 @@
+package Week3.day1.assignment;
+
+public class AxisBank extends BankInfo{
+	
+	public void Desposit() {
+		System.out.println("deposit interest percentage is 15");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		AxisBank Axis = new AxisBank();
+		Axis.Desposit();
+	}
+
+}
---
+++ assignment/BankInfo.java
@@ -0,0 +1,15 @@
+package Week3.day1.assignment;
+
+public class BankInfo {
+	public void Saving()
+	{
+		System.out.println( "Saving plan info");
+	}
+	
+	public void Fixed() {
+		System.out.println("Fixed account");
+	}
+	public void Desposit() {
+		System.out.println("deposit interest percentage is 5");
+	}
+}
